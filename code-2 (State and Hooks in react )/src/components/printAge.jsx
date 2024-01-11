6270aad85c9a9521e8d9ec1f6bb6d72466144636
ShravaniBLagdive <141992595+ShravaniBLagdive@users.noreply.github.com>
2024-01-11 22:41:20 +0530

    Add files via upload

---
+++ code-2 (State and Hooks in react )/src/components/printAge.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+export const PrintAge = () => {
+  return (
+    <div>
+      <h1>Age : {state} </h1>
+      <button onclick={addAge}>add Age </button>
+      <h1>Date 1 feb</h1>
+    </div>
+  );
+};
