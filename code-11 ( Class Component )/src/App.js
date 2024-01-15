95e824fb0e1d8fda7415e60b733c5f75beaa4563
ShravaniBLagdive <141992595+ShravaniBLagdive@users.noreply.github.com>
2024-01-15 16:41:14 +0530

    Add files via upload

---
+++ code-11 ( Class Component )/src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import ClassComponent from "./ClassComponent";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello This is ClassComponent</h1>
+      <ClassComponent />
+    </div>
+  );
+}
